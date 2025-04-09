8d154dc2fda020f2879bf4289a9305aebb243fa6
Bea Rodrigues <116564168+BeaRoodriguees@users.noreply.github.com>
2025-04-09 18:54:56 -0300

    Issue1 - Finalização do Campo de Futebol (#7)
    * feat: add background color (grass)
    
    * feat: update goal dimensions and implement goal drawing
    
    * feat: enhance ball drawing and add small area and mark drawing to field
    
    ---------
    
    Co-authored-by: BeaRoodriguees <bearoodriguees@gotmail.com>

--- soccer/field.py
+++ soccer/field.py
@@ -2,29 +2,42 @@ import numpy as np
 from OpenGL.GL import glPopMatrix, glPushMatrix, glRotatef
 
 from soccer.bresenham import bresenham_circle, bresenham_line
+from OpenGL.GL import glBegin, glEnd, glVertex2f, GL_TRIANGLE_FAN
 
 
 class Field:
     def __init__(self, size_factor: int = 1):
         self.width = size_factor * 90.0
         self.length = size_factor * 120.0
-        self.center_radius = size_factor * 9.15
-        self.big_area_width = size_factor * 50
+        self.center_radius = size_factor * 14.15
+        self.big_area_width = size_factor * 60
         self.big_area_length = size_factor * 40
-        self.small_area_length = size_factor * 5.5
-        self.small_area_width = size_factor * 18.3
-        self.goal_length = size_factor * 2.4
-        self.goal_width = size_factor * 7.3
+        self.small_area_length = size_factor * 15.5
+        self.small_area_width = size_factor * 27.3
+        self.goal_length = size_factor * 8.5
+        self.goal_width = size_factor * 18.3
 
     def draw(self):
         glPushMatrix()
 
         self._draw_field()
         self._draw_center()
-        self._draw_big_area()
 
+        self._draw_big_area()
         glRotatef(180, 1, 0, 0)
         self._draw_big_area()
+
+        self._draw_small_area()
+        glRotatef(180, 1, 0, 0)
+        self._draw_small_area()
+
+        self._draw_goal()
+        glRotatef(180, 1, 0, 0)
+        self._draw_goal()
+
+        self._draw_mark(0, 0)
+        self._draw_mark(0, -3 * self.small_area_length)
+        self._draw_mark(0, 3 * self.small_area_length)
 
         glPopMatrix()
 
@@ -71,3 +84,70 @@ class Field:
         bresenham_line(A, C)
         bresenham_line(A, B)
         bresenham_line(B, D)
+
+    def _draw_small_area(self):
+        A = np.array(
+            [
+                -self.small_area_width // 2,
+                self.length // 2 - self.small_area_length // 2,
+            ],
+            dtype=np.float32,
+        )
+        B = np.array(
+            [
+                self.small_area_width // 2,
+                self.length // 2 - self.small_area_length // 2,
+            ],
+            dtype=np.float32,
+        )
+        C = np.array(
+            [-self.small_area_width // 2, self.length // 2], dtype=np.float32
+        )
+        D = np.array(
+            [self.small_area_width // 2, self.length // 2], dtype=np.float32
+        )
+
+        bresenham_line(A, C)
+        bresenham_line(A, B)
+        bresenham_line(B, D)
+
+    def _draw_goal(self):
+        A = np.array(
+            [
+                -self.goal_width // 2,
+                self.length // 2 + self.goal_length // 2,
+            ],
+            dtype=np.float32
+        )
+        B = np.array(
+            [
+                self.goal_width // 2,
+                self.length // 2 + self.goal_length // 2,
+            ],
+            dtype=np.float32
+        )
+        C = np.array(
+            [-self.goal_width // 2, self.length // 2], dtype=np.float32
+        )
+        D = np.array(
+            [self.goal_width // 2, self.length // 2], dtype=np.float32
+        )
+
+        bresenham_line(A, B)
+        bresenham_line(A, C)
+        bresenham_line(B, D)
+
+    def _draw_mark(self, x, y):
+        CENTER = np.array([x, y], dtype=np.float32)
+        radius = 5
+        num_segments = 100
+
+        glBegin(GL_TRIANGLE_FAN)
+        glVertex2f(CENTER[0], CENTER[1])
+        for i in range(num_segments + 1):
+            angle = 2.0 * np.pi * i / num_segments
+            glVertex2f(
+            CENTER[0] + (radius * np.cos(angle)),
+            CENTER[1] + (radius * np.sin(angle))
+            )
+        glEnd()
